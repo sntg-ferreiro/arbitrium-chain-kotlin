09b1ce5de2f2cd4d707d6cdb5aa5da51f29be3e6
sntg-ferreiro <san.ferreiro92@gmail.com>
2021-11-23 20:34:07 -0300

    minor sonar fix

--- src/main/kotlin/ar/com/arbitrium/chain/ChainApplication.kt
+++ src/main/kotlin/ar/com/arbitrium/chain/ChainApplication.kt
@@ -2,7 +2,6 @@ package ar.com.arbitrium.chain
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class ChainApplication
